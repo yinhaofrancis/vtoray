b598112df7886a9c18e483e1986007b73a8bea66
Francis <1833918721@qq.com>
2021-02-03 02:09:55 +0800

    yts

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ sudo apt install unzip
 sudo apt update
 sudo apt install curl
 sudo curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+sudo bash install-release.sh
 sudo mkdir /etc/v2ray
 git clone https://github.com/yinhaofrancis/vtoray.git
 cd vtoray
-sudo cp myconfig.json /etc/v2ray/config.json
+sudo rm -f /usr/local/etc/v2ray/config.json
+sudo cp myconfig.json /usr/local/etc/v2ray/config.json
