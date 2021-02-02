4ffac51372b505ab8ebc51ef47a1b361acfdb86d
Francis <1833918721@qq.com>
2021-02-03 01:30:05 +0800

    test

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt install git-all
+sudo apt install unzip
+sudo mkdir /etc/v2ray
+git clone https://github.com/yinhaofrancis/vtoray.git
+cd vtoray
+unzip v2ray-linux-64.zip
+cp myconfig.json /etc/v2ray/config.json
