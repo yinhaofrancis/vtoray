c07be5c81137a5856fb2c982597630cc65c71bf7
yinhao <yinhao@5eplay.com>
2021-06-15 10:02:13 +0800

    config

---
+++ config.sh
@@ -0,0 +1,8 @@
+UUID1=$(cat /proc/sys/kernel/random/uuid)
+UUID2=$(cat /proc/sys/kernel/random/uuid)
+
+file=sed -e s/{{uuid1}}/${UUID1}/g -e s/{{uuid2}}/${UUID2}/g myconfig.json 
+
+
+echo ${file}>currentConfig.json
+
