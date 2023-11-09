44e8183686858df9cff542c01d24cf746c316fe8
yinhao <yinhao@5eplay.com>
2023-11-09 19:41:51 +0800

    Update setupcer.sh

--- setupcer.sh
+++ setupcer.sh
@@ -6,14 +6,24 @@ if [ -z "$2" ]; then
     echo "输入证书文件夹 param 2"
     exit 1
 fi
-curl  https://get.acme.sh | sh
-sudo apt-get -y install netcat
-folder_name="$2"
-if [ -d "${folder_name}" ]; then
-    echo "文件夹已存在"
-else
-    mkdir "$folder_name"
-    echo "文件夹已创建"
+if [ ! -f ~/.acme.sh/acme.sh ]; then
+    curl  https://get.acme.sh | sh
+    sudo apt-get -y install netcat
 fi
 
-sudo ~/.acme.sh/acme.sh --installcert -d $1 --fullchainpath $2/v2ray.crt --keypath $2/v2ray.key --ecc
+if [ -f $2/v2ray.crt ]; then
+    sudo ~/.acme.sh/acme.sh --renew -d $1 --force --ecc
+else
+    sudo ~/.acme.sh/acme.sh --issue -d $1 --standalone -k ec-256
+    folder_name="$2"
+    if [ -d "${folder_name}" ]; then
+        echo "文件夹已存在"
+    else
+        mkdir "$folder_name"
+        echo "文件夹已创建"
+    fi
+    sudo ~/.acme.sh/acme.sh --installcert -d $1 --fullchainpath $2/v2ray.crt --keypath $2/v2ray.key --ecc
+fi
+
+
+
