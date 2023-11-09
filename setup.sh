847cbf1531890bb1b1a79161faa899c1d189f6b7
yinhao <yinhao@5eplay.com>
2023-11-09 19:25:40 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ fi
 # sudo curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 sudo curl -O https://raw.githubusercontent.com/yinhaofrancis/fhs-install-v2ray/master/install-release.sh
 sudo bash install-release.sh
-sudo bash setupcur.sh $1 /root/tls
+sudo bash setupcer.sh $1 /root/tls
 bash config.sh /root/tls
 sudo rm -f /usr/local/etc/v2ray/config.json
 sudo cp currentConfig.json /usr/local/etc/v2ray/config.json
