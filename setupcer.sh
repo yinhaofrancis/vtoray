af1f1fbcf53a0241b98c68af9770a3db1ae56bcb
yinhao <yinhao@5eplay.com>
2023-11-10 01:44:48 +0800

    no sudo

--- setupcer.sh
+++ setupcer.sh
@@ -7,14 +7,14 @@ if [ -z "$2" ]; then
     exit 1
 fi
 if [ ! -f ~/.acme.sh/acme.sh ]; then
-    curl  https://get.acme.sh | sh
+    sudo curl  https://get.acme.sh | sh
 fi
 
 if [ -f $2/v2ray.crt ]; then
-    sudo ~/.acme.sh/acme.sh --renew -d $1 --force --ecc
+    ~/.acme.sh/acme.sh --renew -d $1 --force --ecc
 else
-    sudo ~/.acme.sh/acme.sh --register-account -m yinhaogerault@gmail.com
-    sudo ~/.acme.sh/acme.sh --issue -d $1 --standalone -k ec-256
+    ~/.acme.sh/acme.sh --register-account -m yinhaogerault@gmail.com
+    ~/.acme.sh/acme.sh --issue -d $1 --standalone -k ec-256
     folder_name="$2"
     if [ -d "${folder_name}" ]; then
         echo "文件夹已存在"
@@ -22,7 +22,7 @@ else
         mkdir "$folder_name"
         echo "文件夹已创建"
     fi
-    sudo ~/.acme.sh/acme.sh --installcert -d $1 --fullchainpath $2/v2ray.crt --keypath $2/v2ray.key --ecc
+    ~/.acme.sh/acme.sh --installcert -d $1 --fullchainpath $2/v2ray.crt --keypath $2/v2ray.key --ecc
 fi
 
 
